4900c6b65d25c1e3f584874cdbff57fc6a0faeec
Gosselin Noa <no.gosselin2006@gmail.com>
2025-03-15 22:30:30 +0100

    feat: Admin header

--- Frontend/src/components/Shared/AppWrapper.tsx
+++ Frontend/src/components/Shared/AppWrapper.tsx
@@ -3,13 +3,14 @@
 import { useState, useEffect } from "react";
 import AuthModal from "../Auth/AuthModal";
 import { useAuth } from "@/hooks/AuthContext";
+import Header from "./Header";
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
   const { isConnected, setIsConnected } = useAuth();
   const [showModal, setShowModal] = useState(false);
   // eslint-disable-next-line
   const [inputSequence, setInputSequence] = useState("");
-  
+
   useEffect(() => {
     const secretSequence = process.env.NEXT_PUBLIC_ADMIN_SEQUENCE || "admin";
     const secretSequenceLength = secretSequence.length;
@@ -17,11 +18,11 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
     const handleKeyDown = (e: KeyboardEvent) => {
       setInputSequence((prev) => {
         const updatedSequence = (prev + e.key).slice(-secretSequenceLength);
-        
+
         if (updatedSequence === secretSequence) {
           setShowModal(true);
         }
-        
+
         return updatedSequence;
       });
     };
@@ -37,8 +38,14 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
+      <Header />
       {children}
-      {showModal && <AuthModal setShowModal={setShowModal} setIsConnected={setIsConnected} />}
+      {showModal && (
+        <AuthModal
+          setShowModal={setShowModal}
+          setIsConnected={setIsConnected}
+        />
+      )}
     </>
   );
 };
